f3c613b399fd48fceb15790362f7551998b9fc4a
Jeff Shurts <jeff@jeffshurts.com>
2020-11-19 18:46:35 -0600

    Got something working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as Tone from 'tone'
+
+function Key(props) {
+    return (
+        <button className="key-white" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+class Keyboard extends React.Component {
+    constructor(props) {
+        super(props);
+        const toneSynth = new Tone.Synth().toDestination();
+        this.state = {
+            notes: ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
+            synth: toneSynth,
+        };
+    }
+
+    handleClick(note) {
+        const synth = this.state.synth;
+        synth.triggerAttackRelease(note, "5s");
+    }
+
+    renderKey(i) {
+        return (
+            <Key
+                value={this.state.notes[i]}
+                onClick={() => this.handleClick(this.state.notes[i])}
+            />
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderKey(0)}
+                    {this.renderKey(1)}
+                    {this.renderKey(2)}
+                    {this.renderKey(3)}
+                    {this.renderKey(4)}
+                    {this.renderKey(5)}
+`                </div>
+            </div>
+        );
+    }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Keyboard />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
