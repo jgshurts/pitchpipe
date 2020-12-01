79b8e968e600e8f219a9be7f40b84776b20f66fc
Jeff Shurts <jeff@jeffshurts.com>
2020-12-01 14:27:47 -0600

    Ready for initial release to GitHub

--- src/index.js
+++ src/index.js
@@ -5,54 +5,142 @@ import * as Tone from 'tone'
 
 function Key(props) {
     return (
-        <button className="key-white" onClick={props.onClick}>
+        <button
+            className="key-white"
+            onClick={props.onClick}
+            style={{width: props.width}}
+        >
             {props.value}
         </button>
     );
 }
 
 class Keyboard extends React.Component {
-    constructor(props) {
-        super(props);
-        const toneSynth = new Tone.Synth().toDestination();
-        this.state = {
-            notes: ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
-            synth: toneSynth,
-        };
-    }
 
     handleClick(note) {
-        const synth = this.state.synth;
-        synth.triggerAttackRelease(note, "5s");
+        const sampler = new Tone.Sampler({
+            urls: {
+                "E2": "E2.mp3",
+                "A2": "A2.mp3",
+                "D3": "D3.mp3",
+                "G3": "G3.mp3",
+                "B3": "B3.mp3",
+                "E4": "E4.mp3",
+            },
+            release: 1,
+            baseUrl: "http://localhost:3000/samples/guitar/",
+        }).toDestination();
+
+        this.setState( {synth: sampler});
+
+        Tone.loaded().then(() => {
+            this.state.synth.triggerAttackRelease(note, 4);
+            this.state.synth.context.resume();
+        })
     }
 
     renderKey(i) {
+        const keyWidth = 300/this.props.notes.length
         return (
             <Key
-                value={this.state.notes[i]}
-                onClick={() => this.handleClick(this.state.notes[i])}
+                key={i.toString()}
+                value={this.props.notes[i]}
+                onClick={() => this.handleClick(this.props.notes[i])}
+                width={keyWidth}
             />
         );
     }
 
     render() {
+        const keyElements = [];
+        for(let i = 0; i < this.props.notes.length; i++) {
+            keyElements.push(this.renderKey(i));
+        }
+
         return (
-            <div>
-                <div className="board-row">
-                    {this.renderKey(0)}
-                    {this.renderKey(1)}
-                    {this.renderKey(2)}
-                    {this.renderKey(3)}
-                    {this.renderKey(4)}
-                    {this.renderKey(5)}
-`                </div>
-            </div>
+                <div className={"keyboard"}>
+                    {keyElements}
+                </div>
         );
     }
 }
 
+function Instrument(props) {
+    return (
+        <button
+            className="instrument"
+            style={{width: props.width}}
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
+}
+
+
+class Instruments extends React.Component {
+
+    renderInstrument(name) {
+        const buttonWidth = 300/this.props.instruments.size
+
+        return (
+            <Instrument
+                key={name}
+                value={name}
+                onClick={() => this.props.onClick(name)}
+                width={buttonWidth}
+            />
+        );
+    }
+
+    render() {
+        const instrumentButtons = [];
+        for(const name of this.props.instruments.keys()) {
+            instrumentButtons.push(this.renderInstrument(name));
+        }
+
+        return (
+            <div className={"instruments"}>
+                {instrumentButtons}
+            </div>
+        )
+    }
+
+}
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        const instrumentData = new Map([
+            ["Guitar", ['E2', 'A2', 'D3', 'G3', 'B3', 'E4']],
+            ["Mandolin", ['G3', 'D4', 'A4', 'E5']],
+            ["Octave Mandolin", ['G2', 'D3', 'A3', 'E4']]
+        ]);
+
+        this.state = {instruments: instrumentData, selectedInstrument: "Guitar"};
+    }
+
+    handleInstrumentClick(name) {
+        this.setState({selectedInstrument: name});
+    }
+
+    render() {
+        return (
+            <div className={"container"}>
+                <Keyboard notes={this.state.instruments.get(this.state.selectedInstrument)}/>
+                <Instruments instruments={this.state.instruments}
+                             selectedInstrument={this.state.selectedInstrument}
+                             onClick={(name) => this.handleInstrumentClick(name)}
+                />
+            </div>
+        );
+    }
+
+}
+
 
 ReactDOM.render(
-    <Keyboard />,
+    <App/>,
     document.getElementById('root')
 );
