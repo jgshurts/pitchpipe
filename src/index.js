51606b127e080c20a562c17ee8d1fd81f5169012
Jeff Shurts <jeff@jeffshurts.com>
2020-12-02 12:55:10 -0600

    Changed logic to allow for unique samples per instrument. 'default' sampleUrl just routes to Guitar samples. (#2)

--- src/index.js
+++ src/index.js
@@ -17,19 +17,27 @@ function Key(props) {
 
 class Keyboard extends React.Component {
 
+
     async handleClick(note) {
+        let samplesUrl = this.props.instrumentData.samplesUrl;
+        let notes = this.props.instrumentData.notes;
+        // use guitar samples for any instrument set up to use default samples
+        if(samplesUrl === 'default') {
+            samplesUrl = 'guitar';
+            notes = ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'];
+        }
+
+        const baseUrl = "http://localhost:3000/samples/";
+        const fullBaseUrl = baseUrl + samplesUrl + "/";
+        let sampleUrls = {}
+        for(const note of notes) {
+            sampleUrls[note] = note + ".mp3";
+        }
         await Tone.start();
         const sampler = new Tone.Sampler({
-            urls: {
-                "E2": "E2.mp3",
-                "A2": "A2.mp3",
-                "D3": "D3.mp3",
-                "G3": "G3.mp3",
-                "B3": "B3.mp3",
-                "E4": "E4.mp3",
-            },
+            urls: sampleUrls,
             release: 1,
-            baseUrl: "http://localhost:3000/samples/guitar/",
+            baseUrl: fullBaseUrl,
         }).toDestination();
 
         this.setState( {synth: sampler});
@@ -42,12 +50,12 @@ class Keyboard extends React.Component {
     }
 
     renderKey(i) {
-        const keyWidth = 300/this.props.notes.length
+        const keyWidth = 300/this.props.instrumentData.notes.length
         return (
             <Key
                 key={i.toString()}
-                value={this.props.notes[i]}
-                onClick={() => this.handleClick(this.props.notes[i])}
+                value={this.props.instrumentData.notes[i]}
+                onClick={() => this.handleClick(this.props.instrumentData.notes[i])}
                 width={keyWidth}
             />
         );
@@ -55,7 +63,7 @@ class Keyboard extends React.Component {
 
     render() {
         const keyElements = [];
-        for(let i = 0; i < this.props.notes.length; i++) {
+        for(let i = 0; i < this.props.instrumentData.notes.length; i++) {
             keyElements.push(this.renderKey(i));
         }
 
@@ -114,11 +122,10 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
-
         const instrumentData = new Map([
-            ["Guitar", ['E2', 'A2', 'D3', 'G3', 'B3', 'E4']],
-            ["Mandolin", ['G3', 'D4', 'A4', 'E5']],
-            ["Octave Mandolin", ['G2', 'D3', 'A3', 'E4']]
+            ["Guitar", {samplesUrl: "guitar", notes: ['E2', 'A2', 'D3', 'G3', 'B3', 'E4']}],
+            ["Mandolin", {samplesUrl: "mandolin", notes: ['G3', 'D4', 'A4', 'E5']}],
+            ["Octave Mandolin", {samplesUrl: "default", notes: ['G2', 'D3', 'A3', 'E4']}]
         ]);
 
         this.state = {instruments: instrumentData, selectedInstrument: "Guitar"};
@@ -131,7 +138,10 @@ class App extends React.Component {
     render() {
         return (
             <div className={"container"}>
-                <Keyboard notes={this.state.instruments.get(this.state.selectedInstrument)}/>
+                <Keyboard
+                    instrument={this.state.selectedInstrument}
+                    instrumentData={this.state.instruments.get(this.state.selectedInstrument)}
+                />
                 <Instruments instruments={this.state.instruments}
                              selectedInstrument={this.state.selectedInstrument}
                              onClick={(name) => this.handleInstrumentClick(name)}
